6a6fca33c16932bdff809009e957933b31aa80ac
Alekzander Bywater <alek.bywater@gmail.com>
2023-02-03 22:41:49 -0600

    Add files via upload

--- python_EA.py
+++ python_EA.py
@@ -230,6 +230,16 @@ def start_trading():
 
     print("Trading process started...")
 
+
+def stop_trading():
+    """
+    This function stops trading when the stop_button is 
+    pressed.
+    """
+    print("Stopping trading...")
+    global is_trading 
+    is_trading = False
+
 root=tkinter.Tk()
 root.title("Exert Advisor")
 root.geometry("500x500")
@@ -247,7 +257,7 @@ tkinter.Label(root, text="News Events Data:").grid(row=6)
 start_button = tkinter.Button(text='Start', width=25, command=start_trading)   # add start button with command to execute start_trading function when clicked   							    	    	    	    	    	    	        
 stop_button = tkinter.Button(text='Stop', width=25, command=stop_trading)    # add stop button with command to execute stop_trading function when clicked     
 
- # Add label to display results of trading logic implemented in functions  
+# Add label to display results of trading logic implemented in functions  
 result_label = tkinter.Label(text="Result: ").grid(row=3)
 
 instrumentsEntry=tkinter.Entry(root)
@@ -306,7 +316,90 @@ def createTable(root):
     nlpDataEntry3.grid(row=3, column=2)
     return tableFrame
 
+#Function to create dataframe
+def create_dataframe(api, instrument):
+    r = instruments.InstrumentsCandles(instrument=instrument, params={"granularity": "M1"})
+    api.request(r)
+    df = pd.DataFrame(r.response['candles'])
+    df = df.drop(columns='volume')
+    df['time'] = pd.to_datetime(df['time'])  # Converting the time from string to datetime object 
+    return df
+
+    
+#Function to prepare training data for model    
+def prepare_training_data(df): 
+    train_cols = ['time', 'openBid', 'highBid', 'lowBid', 'closeBid']  # Training columns which will be used to train the model 
+    df_train, df_test = chronological.split_into_train_test(df, train_cols)   # Split the dataset into training and test set 
+
+	#Normalize training data using MinMaxScaler					  
+    scaler = MinMaxScaler()                                                   # Instantiate an object of the minmaxscaler class  
+
+    scaled_train = scaler.fit_transform(df[train_cols])                        # Fit and transform the training data using minmaxscaler object    
+
+	#Create input sequence and labels for model     
+    X, y = chronological.create_inputs_andlabels(scaled_train)                  # Create input sequences and labels from normalized training data    
+
+	#Reshape input sequence for model     
+    X = X.reshape(-1, 1 ,4)                                                    # Reshape the inputs in order to fit into LSTM model       
+
+	#Return prepared inputs and labels      
+    return X, y  
+
+    
+#Function to build LSTM model    
+def buildLSTMModel():     
+
+	#Instantiate a Sequential Model Object        
+    model = Sequential()         
+
+	#Add layers to model            
+    model.add(LSTM(50, activation='relu', return_sequences=True))               # Add a layer of 50 neurons with relu activation function         
+
+    model.add(Dropout (0.2))                                                    # Add a dropout layer with 20% neuron dropping rate          
+
+    model.add (LSTM (50, activation='relu'))                                    # Add another layer of 50 neurons with relu activation function       
+
+    model.add (Dense (25))                                                     # Add a Dense Layer with 25 Neurons            
+
+    model.add (Dense (1))                                                      # Output Layer             
+
+     ## Compile Model           
+
+    model.compile(optimizer='adam', loss='mean_squared_error')                  # Compile the model with Adam Optimizer and mean squared error loss function
+
+	#Return Model Object         
+    return model  
+
+
+#Function to run model and predict    
+def runModel(X, y, model): 
+
+	#Train Model       
+    model.fit(X, y, batch_size=1, epochs=1)                                    # Fit the training data into the LSTM Model for 1 epoch  
+
+	#Predict Closing Price of Instrument     
+    prediction = model.predict(X)                                              # Make predictions from the trained model           
+
+	#Return Predictions      
+    return prediction 
 
 
 if __name__ == "__main__":
+    #Create API Object        
+    api = API (access_token=" c546ba5cbc5ff51a3ddff21642565098-3068bb7c8922261d0e820442860f7dea")           # Replace YOURTOKEN with your Oanda V20 access token         
+
+	#Create Dataframe Object       
+    df = create_dataframe (api, "EUR_USD")         # Create a dataframe using the instruments candles endpoint of oandapyV20       
+
+	#Prepare Training Data          
+    X, y = prepare_training_data (df)              # Prepare the training data for input into the LSTM Model            
+
+	#Build LSTM Model        
+    model = buildLSTMModel ()                      # Build an LSTM Model with two layers and 25 neurons in dense layer         
+
+	#Run Model and Predict      
+    predictions = runModel (X, y, model)            # Run the LSTM Model on prepared training data and make predictions              
+
+	#Display Predictions        
+    print (predictions)                            # Display the predictions made by the model
     root.mainloop()
